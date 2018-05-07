2d8266b16d3606fbefa9cecb402b1b7cd93c10a0
Will Ruiz <thisis.willruiz@gmail.com>
2018-05-07 16:09:46 -0700

    Reverse a string recursively

--- recursion/reverse.js
+++ recursion/reverse.js
@@ -4,3 +4,9 @@ Implement a function that will reverse a string recursively.
 reverse('abcdefg')
 => 'gfedcba'
 */
+
+const reverse = (str) => {
+  return str.length === 1 ? str : ( str[str.length - 1] + reverse(str.slice(0, str.length - 1)) );
+};
+
+console.log( reverse('abcdefg') );
